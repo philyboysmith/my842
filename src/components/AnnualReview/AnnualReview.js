46aa9907685ded1eb7e68037780c5478ab1c307d
Kamuela Franco <kamuela.franco@gmail.com>
2017-01-31 18:09:23 +0000

    Added props to AnnualReview children

--- src/components/AnnualReview/AnnualReview.js
+++ src/components/AnnualReview/AnnualReview.js
@@ -6,16 +6,20 @@ import BudgetPlanner from './BudgetPlanner';
 import FamilyTree from './FamilyTree';
 import RiskProfile from './RiskProfile';
 
-function AnnualReview(props) {
+function AnnualReview({ annualReviewResponse }) {
   return (
     <div>
-      <AssetsAndLiabilities />
-      <FamilyTree />
-      <BudgetPlanner />
-      <RiskProfile />
-      <Availability />
+      <AssetsAndLiabilities data={annualReviewResponse.assets_and_liabilities} />
+      <FamilyTree data={annualReviewResponse.family_tree} />
+      <BudgetPlanner data={annualReviewResponse.budget_planner} />
+      <RiskProfile data={annualReviewResponse.risk_profile} />
+      <Availability data={annualReviewResponse.availability} />
     </div>
   );
 }
+
+AnnualReview.propTypes = {
+  annualReviewResponse: React.PropTypes.object,
+};
 
 export default AnnualReview;
