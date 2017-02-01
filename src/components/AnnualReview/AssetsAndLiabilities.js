b7d7dfa23b616a14d3021ee054cd83d19b193faa
Kamuela Franco <kamuela.franco@gmail.com>
2017-02-01 04:51:03 +0000

    Refactored AssetsAndLiabilities component to render elements based on data

--- src/components/AnnualReview/AssetsAndLiabilities.js
+++ src/components/AnnualReview/AssetsAndLiabilities.js
@@ -2,96 +2,39 @@ import React from 'react';
 
 import RequiredChanges from './RequiredChanges';
 
-function AssetsAndLiabilities({ data }) {
+function renderListItem(item, title) {
   return (
     <div>
-      <h1>Assets and Liability Statement</h1>
-      <div>
-        <p>Etc</p>
-        <h2>Properties</h2>
-        <ul className="al_list">
-          <li className="al_list__item">
-            <div>
-              <h3>17 The Meadway</h3>
-              <p>Main residence (UK)</p>
-              <p><strong>£1,000,000</strong></p>
-            </div>
-            <RequiredChanges />
-          </li>
-        </ul>
-      </div>
-      <div>
-        <h2>Pensions</h2>
-        <ul className="al_list">
-          <li className="al_list__item">
-            <div>
-              <h3>Trans Oceanic Meat Co Pension Scheme</h3>
-              <p>Small-Self Administered Scheme</p>
-              <p><strong>£5,491,786</strong></p>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <h2>Income</h2>
-        <ul className="al_list">
-          <li className="al_list__item">
-            <div>
-              <h3>Business income</h3>
-              <p><strong>£10,000,000</strong></p>
-            </div>
-          </li>
-        </ul>
-      </div>
-
-      <div>
-        <h2>Liquid Assets</h2>
-        <ul className="al_list">
-          <li className="al_list__item">
-            <div>
-              <h3>Charles Stanley</h3>
-              <p>General Investment Account</p>
-              <p><strong>£2,000,000</strong></p>
-            </div>
-          </li>
-          <li className="al_list__item">
-            <div>
-              <h3>Charles Stanley</h3>
-              <p>General Investment Account</p>
-              <p><strong>£2,000,000</strong></p>
-            </div>
-          </li>
-          <li className="al_list__item">
-            <div>
-              <h3>Charles Stanley</h3>
-              <p>General Investment Account</p>
-              <p><strong>£2,000,000</strong></p>
-            </div>
-          </li>
-        </ul>
-      </div>
-
-      <div>
-        <h2>Current Accounts</h2>
-        <ul className="al_list">
-          <li className="al_list__item">
-            <div>
-              <h3>Coutts</h3>
-              <p>Current account</p>
-              <p><strong>£2,000,000</strong></p>
-            </div>
-          </li>
-          <li className="al_list__item">
-            <div>
-              <h3>HSBC Savings Account</h3>
-              <p><strong>£1,000,000</strong></p>
-            </div>
-          </li>
-        </ul>
-      </div>
+      <h2>{title}</h2>
+      <ul className="al_list">
+        <li className="al_list__item">
+          <div>
+            <h3>17 The Meadway</h3>
+            <p>Main residence (UK)</p>
+            <p><strong>£1,000,000</strong></p>
+          </div>
+          <RequiredChanges />
+        </li>
+      </ul>
     </div>
   );
 }
 
-export default AssetsAndLiabilities;
+function AssetsAndLiabilities({ data }) {
+  return (
+    <div>
+      <h1>Assets and Liabilities Statement</h1>
+      {renderListItem(data.properties, 'Properties')}
+      {renderListItem(data.pensions, 'Pensions')}
+      {renderListItem(data.income, 'Income')}
+      {renderListItem(data.liquid_assets, 'Liquid Assets')}
+      {renderListItem(data['current-accounts'], 'Current Accounts')}
+    </div>
+  );
+}
 
+AssetsAndLiabilities.propTypes = {
+  data: React.PropTypes.object,
+};
+
+export default AssetsAndLiabilities;
