358a89aa59f64d5d7b5148b3ce8adf7209b32a45
Kamuela Franco <kamuela.franco@gmail.com>
2017-02-02 13:53:53 +0000

    Updated FamilyTree shape

--- src/components/AnnualReview/FamilyTree.js
+++ src/components/AnnualReview/FamilyTree.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 function FamilyTree({ data }) {
   let parsedData = data;
-  if (data.length === 0) {
+  if (Object.keys(data).length === 0) {
     parsedData = 'No Family Tree data';
   }
   return (
     <div>
       <h1>Family Tree</h1>
-      <p>{parsedData}</p>
+      <p>{JSON.stringify(parsedData)}</p>
     </div>
   );
 }
 
 FamilyTree.propTypes = {
-  data: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
+  data: React.PropTypes.object.isRequired,
 };
 
 export default FamilyTree;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react';
 
 export default class Login extends Component {
 
@@ -6,52 +6,52 @@ export default class Login extends Component {
     e.preventDefault();
     const email = this.refs.email.value;
     const password = this.refs.password.value;
-    const creds = { email: email.trim(), password: password.trim() }
+    const creds = { email: email.trim(), password: password.trim() };
     this.props.loginUser(creds);
   }
 
   render() {
-    const { errorMessage } = this.props.auth
+    const { errorMessage } = this.props.auth;
 
     return (
       <div className="single-widget-container" key="Login">
         <section className="widget login-widget">
           <header className="text-align-center">
-              <h4>Account login</h4>
+            <h4>Account login</h4>
           </header>
-            <form ref="loginForm" className="form-horizontal" onSubmit={this.handleSubmit.bind(this)}>
-              <div className="form-group">
-                <div className="input-group">
-                  <span className="input-group-addon">
-                    <i className="fa fa-user"></i>
-                  </span>
-                  <input type="text" className="form-control input-transparent"  ref="email" placeholder="email" />
-                </div>
+          <form ref="loginForm" className="form-horizontal" onSubmit={this.handleSubmit.bind(this)}>
+            <div className="form-group">
+              <div className="input-group">
+                <span className="input-group-addon">
+                  <i className="fa fa-user" />
+                </span>
+                <input type="text" className="form-control input-transparent" ref="email" placeholder="email" />
               </div>
-              <div className="form-group">
-                <div className="input-group">
-                  <span className="input-group-addon">
-                    <i className="fa fa-lock"></i>
-                  </span>
-                  <input type="password" className="form-control input-transparent"  ref="password" placeholder="password" />
-                </div>
+            </div>
+            <div className="form-group">
+              <div className="input-group">
+                <span className="input-group-addon">
+                  <i className="fa fa-lock" />
+                </span>
+                <input type="password" className="form-control input-transparent" ref="password" placeholder="password" />
               </div>
-              <div className="row">
+            </div>
+            <div className="row">
 
-               <button type="submit" className="btn btn-primary">Submit</button>
-               {errorMessage &&
+              <button type="submit" className="btn btn-primary">Submit</button>
+              {errorMessage &&
                 <span className="btn btn-link fade">{errorMessage}</span>
               }
-              </div>
-            </form>
+            </div>
+          </form>
 
         </section>
       </div>
-    )
+    );
   }
 
 }
 
 Login.propTypes = {
-  errorMessage: PropTypes.string
-}
+  errorMessage: PropTypes.string,
+};
