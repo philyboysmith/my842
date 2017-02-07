21863790d65e4d3cefea003f15fffe2bf75dff34
Kamuela Franco <kamuela.franco@gmail.com>
2017-02-07 00:07:30 +0000

    Pleased airbnb

--- src/index.js
+++ src/index.js
@@ -34,23 +34,23 @@ const router = (
   <Provider store={store}>
     <Router history={history}>
       <Route path="/" component={App}>
-        <IndexRoute component={Clients} title="Clients"></IndexRoute>
-        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID" component={Client} title=""></Route>
-        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/meetings" component={Meetings} title="My Meetings"></Route>
-        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/children" component={Children} title="My Children and Dependents"></Route>
-        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/contacts" component={Contacts} title="My Personal Details"></Route>
-        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/properties" component={Properties} title="My Homes and Properties" business_title="My Leasehold / Freehold Property Details"></Route>
-        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/advisers" component={Advisers} title="My Professional Advisers"></Route>
-        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/employments" component={Employments} title="My Employments and Tax Details"></Route>
-        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/loans" component={Loans} title="My Loans and Mortgages" business_title="My Business Loans and Mortgages"></Route>
-        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/business" component={Business} title="My Business Details"></Route>
-        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/successions" component={Successions} title="My Succesion Planning"></Route>
-        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/benefits" component={Benefits} title="My Employee Benefits"></Route>
-        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/estates" component={Estates} title="My Estate Planning"></Route>
-        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/expenditures" component={Expenditures} title="My Expenditures"></Route>
-        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/team" component={Team} title="My Team"></Route>
-        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/documents" component={Documents} title="My Documents"></Route>
-        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/annual-review" component={AnnualReview} title="Annual Review"></Route>
+        <IndexRoute component={Clients} title="Clients" />
+        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID" component={Client} title="" />
+        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/meetings" component={Meetings} title="My Meetings" />
+        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/children" component={Children} title="My Children and Dependents" />
+        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/contacts" component={Contacts} title="My Personal Details" />
+        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/properties" component={Properties} title="My Homes and Properties" business_title="My Leasehold / Freehold Property Details" />
+        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/advisers" component={Advisers} title="My Professional Advisers" />
+        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/employments" component={Employments} title="My Employments and Tax Details" />
+        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/loans" component={Loans} title="My Loans and Mortgages" business_title="My Business Loans and Mortgages" />
+        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/business" component={Business} title="My Business Details" />
+        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/successions" component={Successions} title="My Succesion Planning" />
+        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/benefits" component={Benefits} title="My Employee Benefits" />
+        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/estates" component={Estates} title="My Estate Planning" />
+        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/expenditures" component={Expenditures} title="My Expenditures" />
+        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/team" component={Team} title="My Team" />
+        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/documents" component={Documents} title="My Documents" />
+        <Route onEnter={denyWithoutAuthentication} path="/clients/:clientID/annual-review" component={AnnualReview} title="Annual Review" />
       </Route>
     </Router>
   </Provider>
