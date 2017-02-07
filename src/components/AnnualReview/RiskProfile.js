77c21f028bfd517bd19517ee74c4e1c7a8bb5a05
Kamuela Franco <kamuela.franco@gmail.com>
2017-02-07 10:30:52 +0000

    Refactored RiskProfile and added propTypes definition

--- src/components/AnnualReview/RiskProfile.js
+++ src/components/AnnualReview/RiskProfile.js
@@ -2,48 +2,49 @@ import React from 'react';
 import Reactable from 'reactable';
 
 function RiskProfile({ data }) {
-
-
-  const Table = Reactable.Table,
-            Tr = Reactable.Tr,
-            Td = Reactable.Td,
-            Thead = Reactable.Thead,
-            Th = Reactable.Th;
+  const Table = Reactable.Table;
+  const Tr = Reactable.Tr;
+  const Td = Reactable.Td;
+  const Thead = Reactable.Thead;
+  const Th = Reactable.Th;
 
   return (
     <div>
       <h1>Risk Profile</h1>
       {data &&
-            data.map((contact,i) =>
-            	<div key={i}>
-            		<h3>{contact.contact_name}</h3>
-            		<table className="table">
-            			<tbody>
-            			{
-            				contact.questions.map((question,i) =>
-            					<tr key={i}>
-            						<td width="400">
-            							{i+1}. {question.body}
-            						</td>
-            						<td>
-            							<ol>
-            							{
-            								question.choices.map((choice,choice_i) =>
-            								<li key={choice_i}><label><input type="radio" name={'question[' + i + ']'} value={choice_i}/> {choice_i+1}. {choice.body}</label></li>
-            							)}
-        								</ol>
-            						</td>
-            					</tr>
-            			)}
-						</tbody>
-            		</table>
-            	</div>
-
-		      
+        data.map((contact, i) =>
+          <div key={i}>
+            <h3>{contact.contact_name}</h3>
+            <table className="table">
+              <tbody>
+                {contact.questions.map((question, j) =>
+                  <tr key={i}>
+                    <td width="400">
+                      {j + 1}. {question.body}
+                    </td>
+                    <td>
+                      <ol>
+                        {question.choices.map((choice, choiceI) =>
+                          <li key={choiceI}>
+                            <label htmlFor={choice}>
+                              <input type="radio" name={`question[${i}]`} value={choiceI} /> {choiceI + 1}. {choice.body}
+                            </label>
+                          </li>,
+                        )}
+                      </ol>
+                    </td>
+                  </tr>,
+                   )}
+              </tbody>
+            </table>
+          </div>,
       )}
-
     </div>
   );
 }
+
+RiskProfile.propTypes = {
+  data: React.PropTypes.object.isRequired,
+};
 
 export default RiskProfile;
