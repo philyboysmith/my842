617bec6b3c7a59fe0c6fa84c8acd512c7ece2ed2
Kamuela Franco <kamuela.franco@gmail.com>
2017-02-07 09:41:54 +0000

    FamilyTree aesthetic refactors

--- src/components/AnnualReview/FamilyTree.js
+++ src/components/AnnualReview/FamilyTree.js
@@ -6,29 +6,28 @@ function FamilyTree({ data }) {
       <h1>Family tree</h1>
       <ul className="clearfix">
         {data.parents.map((parent, i) =>
-          <li key={i} className={"client "+ (parent.parents.length > 0 ? 'with-parents' : '')}>
-          {parent.name}
-          <ul className="grandparents">
-          {parent.parents.map((grandparent, i) =>
-              <li key={i}>
-                {grandparent.name}
-              </li>
-            )}
-          </ul>
-          <ul className="siblings">
-          {parent.siblings.map((sibling, i) =>
-              <li key={i}>
-                {sibling.name}
-              </li>
-            )}
-          </ul>
-          </li>
+          <li key={i} className={'client ' + (parent.parents.length > 0 ? 'with-parents' : '')}>
+            {parent.name}
+            <ul className="grandparents">
+              {parent.parents.map((grandparent, j) =>
+                <li key={j}>
+                  {grandparent.name}
+                </li>,
+                )}
+            </ul>
+            <ul className="siblings">
+              {parent.siblings.map((sibling, j) =>
+                <li key={j}>
+                  {sibling.name}
+                </li>,
+                )}
+            </ul>
+          </li>,
         )}
       </ul>
     </div>
   );
 }
-
 
 FamilyTree.propTypes = {
   data: React.PropTypes.object.isRequired,
