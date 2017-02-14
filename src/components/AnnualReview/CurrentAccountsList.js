09ca47d3cb66debe410722f0ead6b6bb8b0a38ce
Kamuela Franco <kamuela.franco@gmail.com>
2017-02-14 05:44:07 +0000

    CurrentAccountsList now displays all data

--- src/components/AnnualReview/CurrentAccountsList.js
+++ src/components/AnnualReview/CurrentAccountsList.js
@@ -6,9 +6,23 @@ function CurrentAccountsList({ data }) {
   return (
     <div>
       <h2>Current Accounts</h2>
-      <RequiredChanges onApproved={() => false} onRevision={() => false} />
+      <ul className="al_list">
+        {data.map(({ approved, description, notes, title, uid, value }) => (
+          <li className="al_list__item" key={uid}>
+            <p>Title: {title}</p>
+            <p>Description: {description}</p>
+            <p>Value: {value}</p>
+            <p>Notes: {notes}</p>
+            <RequiredChanges isApproved={approved} onApproved={() => {}} onRevision={() => {}} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+CurrentAccountsList.propTypes = {
+  data: React.PropTypes.array.isRequired,
+};
 
 export default CurrentAccountsList;
