21e357b20cd6ce7b85730ee534a171a798ddd44a
Kamuela Franco <kamuela.franco@gmail.com>
2017-02-14 05:38:13 +0000

    Removed deprecated renderListItem function from IncomeList component

--- src/components/AnnualReview/IncomeList.js
+++ src/components/AnnualReview/IncomeList.js
@@ -2,34 +2,6 @@ import React from 'react';
 
 import RequiredChanges from './RequiredChanges';
 
-function renderListItem(item, title) {
-  const rendered = [];
-  if (!Array.isArray(item)) {
-    if (Object.keys(item).length === 0) {
-
-    } else {
-      rendered.push(
-        <div key={item.name}>
-          <h3>{item.name}</h3>
-          <p>Notes: {item.notes}</p>
-          <p>Value:<strong>£{item.value}</strong></p>
-        </div>,
-      );
-    }
-  }
-  return (
-    <div>
-      <h2>{title}</h2>
-      <ul className="al_list">
-        <li className="al_list__item">
-          {rendered}
-          <RequiredChanges onApproved={() => false} onRevision={() => false} />
-        </li>
-      </ul>
-    </div>
-  );
-}
-
 function IncomeList({ data }) {
   const { approved, name, notes, value } = data;
   return (
