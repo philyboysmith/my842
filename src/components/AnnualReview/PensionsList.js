3cda6c0ceba9c2ff159c583f8166ab2ad141fcaf
Kamuela Franco <kamuela.franco@gmail.com>
2017-02-14 05:27:32 +0000

    Added classes to PensionsList and PropertiesList output

--- src/components/AnnualReview/PensionsList.js
+++ src/components/AnnualReview/PensionsList.js
@@ -6,15 +6,17 @@ function PensionsList({ data }) {
   return (
     <div>
       <h2>Pensions</h2>
-      {data.map(({ approved, description, notes, title, uid, value }) => (
-        <div key={uid}>
-          <p>Title: {title}</p>
-          <p>Description: {description}</p>
-          <p>Value: {value}</p>
-          <p>Notes: {notes}</p>
-          <RequiredChanges isApproved={approved} onApproved={() => {}} onRevision={() => {}} />
-        </div>
-      ))}
+      <ul className="al_list">
+        {data.map(({ approved, description, notes, title, uid, value }) => (
+          <li className="al_list__item" key={uid}>
+            <p>Title: {title}</p>
+            <p>Description: {description}</p>
+            <p>Value: {value}</p>
+            <p>Notes: {notes}</p>
+            <RequiredChanges isApproved={approved} onApproved={() => {}} onRevision={() => {}} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/components/AnnualReview/PropertiesList.js
+++ src/components/AnnualReview/PropertiesList.js
@@ -6,15 +6,17 @@ function PropertiesList({ data }) {
   return (
     <div>
       <h2>Properties</h2>
+      <ul className="al_list">
       {data.map(({ approved, description, notes, title, uid, value }) => (
-        <div key={uid}>
-          <p>Title: {title}</p>
-          <p>Description: {description}</p>
-          <p>Value: {value}</p>
-          <p>Notes: {notes}</p>
-          <RequiredChanges isApproved={approved} onApproved={() => {}} onRevision={() => {}} />
-        </div>
+          <li className="al_list__item" key={uid}>
+            <p>Title: {title}</p>
+            <p>Description: {description}</p>
+            <p>Value: {value}</p>
+            <p>Notes: {notes}</p>
+            <RequiredChanges isApproved={approved} onApproved={() => {}} onRevision={() => {}} />
+          </li>
       ))}
+      </ul>
     </div>
   );
 }
