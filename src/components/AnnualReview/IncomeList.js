95a79d3cf589f3cbbb4e5b6e60dec44c53e56145
Kamuela Franco <kamuela.franco@gmail.com>
2017-02-14 05:32:11 +0000

    IncomeList now displays all data

--- src/components/AnnualReview/IncomeList.js
+++ src/components/AnnualReview/IncomeList.js
@@ -31,12 +31,24 @@ function renderListItem(item, title) {
 }
 
 function IncomeList({ data }) {
+  const { approved, name, notes, value } = data;
   return (
     <div>
       <h2>Income</h2>
-      <RequiredChanges onApproved={() => false} onRevision={() => false} />
+      <ul className="al_list">
+        <li className="al_list__item">
+          <h4>{name}</h4>
+          <p>Notes: {notes}</p>
+          <p>Value: £{value}</p>
+          <RequiredChanges isApproved={approved} onApproved={() => {}} onRevision={() => {}} />
+        </li>
+      </ul>
     </div>
   );
 }
+
+IncomeList.propTypes = {
+  data: React.PropTypes.object.isRequired,
+};
 
 export default IncomeList;
