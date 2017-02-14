15f479e8c1d692fb7614c4f085e8719039ef85e9
Kamuela Franco <kamuela.franco@gmail.com>
2017-02-14 05:41:37 +0000

    LiquidAssetsList now displays all data

--- src/components/AnnualReview/LiquidAssetsList.js
+++ src/components/AnnualReview/LiquidAssetsList.js
@@ -6,9 +6,23 @@ function LiquidAssetsList({ data }) {
   return (
     <div>
       <h2>Liquid Assets</h2>
-      <RequiredChanges onApproved={() => false} onRevision={() => false} />
+      <ul className="al_list">
+        {data.map(({ approved, description, notes, title, uid, value }) => (
+          <li className="al_list__item" key={uid}>
+            <p>Title: {title}</p>
+            <p>Description: {description}</p>
+            <p>Value: {value}</p>
+            <p>Notes: {notes}</p>
+            <RequiredChanges isApproved={approved} onApproved={() => {}} onRevision={() => {}} />
+          </li>
+          ))}
+      </ul>
     </div>
   );
 }
+
+LiquidAssetsList.propTypes = {
+  data: React.PropTypes.array.isRequired,
+};
 
 export default LiquidAssetsList;
