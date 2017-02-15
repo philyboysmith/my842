f1c3a67c74282cc4109c82009c477ac7b755fe56
Phil Smith <phil@amillionmonkeys.co.uk>
2017-02-15 12:19:04 +0000

    update styles

--- src/components/AnnualReview/FamilyTree.js
+++ src/components/AnnualReview/FamilyTree.js
@@ -5,29 +5,39 @@ function FamilyTree({ data }) {
   return (
     
 
-    <div className="family-tree">
+    <div id="family-tree-wrapper">
       <h1>Family tree</h1>
-      <ul className="clearfix">
-        {data.parents.map((parent, i) =>
-          <li key={i} className={'client ' + (parent.parents.length > 0 ? 'with-parents' : '')}>
-            {parent.name}
-            <ul className="grandparents">
-              {parent.parents.map((grandparent, j) =>
-                <li key={j}>
-                  {grandparent.name}
-                </li>,
-                )}
-            </ul>
-            <ul className="siblings">
-              {parent.siblings.map((sibling, j) =>
-                <li key={j}>
-                  {sibling.name}
-                </li>,
-                )}
-            </ul>
-          </li>,
-        )}
-      </ul>
+      <div className="family-tree" id="family-tree">
+        <ul className="clients">
+          {data.parents.map((parent, i) =>
+            <li key={i} className={'client ' + (parent.parents.length > 0 ? 'with-parents' : '')}>
+              <span className="person">{parent.name}</span>
+              <ul className="parents">
+                {parent.parents.map((grandparent, j) =>
+                  <li key={j}>
+                    <span className="person">{parent.name}</span>
+                  </li>,
+                  )}
+              </ul>
+              <ul className="siblings">
+                {parent.siblings.map((sibling, j) =>
+                  <li key={j}>
+                    <span className="person">{parent.name}</span>
+                  </li>,
+                  )}
+              </ul>
+            </li>,
+          )}
+        </ul>
+        <ul className="children generations-1">
+          {data.children.map((parent, i) =>
+            <li key={i} className={'client ' + (parent.children.length > 0 ? 'with-child' : '')}>
+              <span className="person">{parent.name}</span>
+              
+            </li>,
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
