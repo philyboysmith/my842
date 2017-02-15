6d7cc0ebfb77bc3c914a9855a78bf028044243bb
Kamuela Franco <kamuela.franco@gmail.com>
2017-02-15 13:47:31 +0000

    Refactored Breadcrumbs and added propTypes validation

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -1,24 +1,29 @@
 import React from 'react';
 
-import {Link} from 'react-router';
+import { Link } from 'react-router';
 
-class Breadcrumbs extends React.Component {
-
-
-	render() {
-
-		return (
-			<div key="Breadcrumb" className="row">
-				<ol className="breadcrumb">
-					<li><Link to={`/`}>Home</Link></li>
-					{this.props.clients.activeClient &&
-						<li><Link to={`/clients/${this.props.clients.activeClient.id}`}>{this.props.clients.activeClient.name}</Link></li>
-					}
-					<li>{this.props.title}</li>
-				</ol>
-			</div>
-
-		)
-	}
+function Breadcrumbs({ clients, title }) {
+  return (
+    <div key="Breadcrumb" className="row">
+      <ol className="breadcrumb">
+        <li><Link to={'/'}>Home</Link></li>
+        {clients.activeClient &&
+          <li><Link to={`/clients/${clients.activeClient.id}`}>{clients.activeClient.name}</Link></li>
+        }
+        <li>{title}</li>
+      </ol>
+    </div>
+  );
 }
+
+Breadcrumbs.propTypes = {
+  clients: React.PropTypes.shape({
+    activeClient: React.PropTypes.object,
+    data: React.PropTypes.array,
+    isFetching: React.PropTypes.bool,
+    isLoaded: React.PropTypes.bool,
+  }).isRequired,
+  title: React.PropTypes.string.isRequired,
+};
+
 export default Breadcrumbs;
--- src/components/Client.js
+++ src/components/Client.js
@@ -5,15 +5,14 @@ export default class Client extends React.Component {
 
 
   componentDidMount() {
-  	if(this.props.auth.isAuthenticated) this.props.setActiveClient(this.props.clients.data, this.props.params.clientID)
+  	if (this.props.auth.isAuthenticated) this.props.setActiveClient(this.props.clients.data, this.props.params.clientID);
   }
 
-	render() {
-
-		return (
-			<div key="Client">
-        		<h1>Hello world</h1>
-			</div>
-		)
-	}
-};
+  render() {
+    return (
+      <div key="Client">
+        <h1>Hello world</h1>
+      </div>
+    );
+  }
+}
