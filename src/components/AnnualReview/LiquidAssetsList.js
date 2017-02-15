caaa25f3222e32516c0dfa487018d2a92f9edc84
Phil Smith <phil@amillionmonkeys.co.uk>
2017-02-15 11:23:08 +0000

    correct styling

--- src/components/AnnualReview/LiquidAssetsList.js
+++ src/components/AnnualReview/LiquidAssetsList.js
@@ -9,10 +9,10 @@ function LiquidAssetsList({ data }) {
       <ul className="al_list">
         {data.map(({ approved, description, notes, title, uid, value }) => (
           <li className="al_list__item" key={uid}>
-            <p>Title: {title}</p>
-            <p>Description: {description}</p>
-            <p>Value: {value}</p>
-            <p>Notes: {notes}</p>
+            <div>
+              <h3>{title}</h3>
+              <p>{description}</p>
+            </div>
             <RequiredChanges isApproved={approved} onApproved={() => {}} onRevision={() => {}} />
           </li>
           ))}
