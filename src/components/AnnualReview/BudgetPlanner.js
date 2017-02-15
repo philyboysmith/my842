f21ba29e6f875c39d651e79614298416c9988544
Kamuela Franco <kamuela.franco@gmail.com>
2017-02-15 12:48:50 +0000

    BudgetPlanner now has proper propType validation

--- src/components/AnnualReview/BudgetPlanner.js
+++ src/components/AnnualReview/BudgetPlanner.js
@@ -5,41 +5,55 @@ function BudgetPlanner({ data }) {
   return (
     <div>
       <h1>Budget planner</h1>
-      {data &&
-        data.map((group, i) =>
-          <Table className="table" key={i}>
-            <Thead>
-              <Th column="title" width="400">
-                <strong className="name-header">{group.group}</strong>
-              </Th>
-              <Th column="current_lifestyle">Current lifestyle</Th>
-              <Th column="survivors_lifestyle">Survivor's lifestyle</Th>
-              <Th column="at_selected_retirement">At selected retirement</Th>
-              <Th column="post_age_75_80">Post Age 75 / 80</Th>
+      {data.map((group, i) =>
+        <Table className="table" key={i}>
+          <Thead>
+            <Th column="title" width="400">
+              <strong className="name-header">{group.group}</strong>
+            </Th>
+            <Th column="current_lifestyle">Current lifestyle</Th>
+            <Th column="survivors_lifestyle">Survivor's lifestyle</Th>
+            <Th column="at_selected_retirement">At selected retirement</Th>
+            <Th column="post_age_75_80">Post Age 75 / 80</Th>
 
-            </Thead>
-            {group.categories &&
-              group.categories.map((category, j) =>
-                <Tr key={j}>
-                  <Td column="title">{category.title}</Td>
-                  <Td column="current_lifestyle">
-                    <input className="input-transparent form-control" name="current_lifestyle" placeholder="50" />
-                  </Td>
-                  <Td column="survivors_lifestyle">
-                    <input className="input-transparent form-control" name="survivors_lifestyle" placeholder="50" />
-                  </Td>
-                  <Td column="at_selected_retirement">
-                    <input className="input-transparent form-control" name="at_selected_retirement" placeholder="50" />
-                  </Td>
-                  <Td column="post_age_75_80">
-                    <input className="input-transparent form-control" name="post_age_75_80" placeholder="50" />
-                  </Td>
-                </Tr>,
+          </Thead>
+          {group.categories.map((category, j) =>
+            <Tr key={j}>
+              <Td column="title">{category.title}</Td>
+              <Td column="current_lifestyle">
+                <input className="input-transparent form-control" name="current_lifestyle" placeholder="50" />
+              </Td>
+              <Td column="survivors_lifestyle">
+                <input className="input-transparent form-control" name="survivors_lifestyle" placeholder="50" />
+              </Td>
+              <Td column="at_selected_retirement">
+                <input className="input-transparent form-control" name="at_selected_retirement" placeholder="50" />
+              </Td>
+              <Td column="post_age_75_80">
+                <input className="input-transparent form-control" name="post_age_75_80" placeholder="50" />
+              </Td>
+            </Tr>,
               )}
-          </Table>,
+        </Table>,
       )}
     </div>
   );
 }
+
+BudgetPlanner.propTypes = {
+  data: React.PropTypes.arrayOf(React.PropTypes.shape({
+    categories: React.PropTypes.arrayOf(React.PropTypes.shape({
+      approved: React.PropTypes.bool,
+      at_selected_retirement: React.PropTypes.string,
+      current_lifestyle: React.PropTypes.string,
+      notes: React.PropTypes.string,
+      post_age_75_80: React.PropTypes.string,
+      survivors_lifestyle: React.PropTypes.string,
+      title: React.PropTypes.string,
+      uid: React.PropTypes.string,
+    })),
+    group: React.PropTypes.string,
+  })).isRequired,
+};
 
 export default BudgetPlanner;
