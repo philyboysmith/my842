285033a7c84b524746b5c5081f05d8086dc38f51
Kamuela Franco <kamuela.franco@gmail.com>
2017-02-23 01:29:31 +0000

    Small refactors to Meetings

--- src/components/Meetings.js
+++ src/components/Meetings.js
@@ -1,27 +1,26 @@
 import React from 'react';
-import Reactable from 'reactable';
+import { Table, Tr } from 'reactable';
 
-import {humanDate} from '../helpers';
+import { humanDate } from '../helpers';
 
 export default class Meetings extends React.Component {
 
-	render() {
+  render() {
+    const { meetings } = this.props.clients.activeClient;
 
-    const {meetings} =  this.props.clients.activeClient;
-
-    const Table = Reactable.Table,
-          Tr = Reactable.Tr;
-
-		return (
-			<div key="Meetings">
-            <Table className="table">
-                {meetings &&
-                  meetings.map((meeting,i) =>
-                  <Tr key={i} className="special-row"
-                    data={{ 'Date': humanDate(meeting.date), 'Time': meeting.time, 'Location': meeting.location, 'Type': meeting.type, 'Individuals present': meeting.individuals_present, 'Notes': meeting.notes }} />
+    return (
+      <div key="Meetings">
+        <Table className="table">
+          {meetings &&
+                  meetings.map((meeting, i) =>
+                    <Tr
+                      key={i} className="special-row"
+                      data={{ Date: humanDate(meeting.date), Time: meeting.time, Location: meeting.location, Type: meeting.type, 'Individuals present': meeting.individuals_present, Notes: meeting.notes }}
+                    />,
                 )}
-            </Table>
-			</div>
-		)
-	}
-};
+        </Table>
+      </div>
+    );
+  }
+}
+
