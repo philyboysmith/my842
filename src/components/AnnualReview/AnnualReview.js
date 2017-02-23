2e01f1aa3105ad2c861b9cddebb7a79884351cb0
Kamuela Franco <kamuela.franco@gmail.com>
2017-02-23 19:13:49 +0000

    Refactored AnnualReview with lifecycle method

--- src/components/AnnualReview/AnnualReview.js
+++ src/components/AnnualReview/AnnualReview.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import pure from 'recompose/pure';
@@ -9,44 +9,50 @@ import BudgetPlanner from './BudgetPlanner';
 import FamilyTree from './FamilyTree';
 import RiskProfile from './RiskProfile';
 
-function AnnualReview({ annualReviewData }) {
-  if (!annualReviewData) {
-    return null;
-  }
-  return (
-    <Tabs>
-      <TabList className="nav nav-tabs">
-        <Tab key="Introduction"><span>Introduction</span></Tab>
-        <Tab key="AssetsAndLiabilities"><span>Assets and Liability Statement</span></Tab>
-        <Tab key="FamilyTree"><span>Family Tree</span></Tab>
-        <Tab key="BudgetPlanner"><span>Budget Planner</span></Tab>
-        <Tab key="RiskProfile"><span>Risk Profile</span></Tab>
-        <Tab key="Availability"><span>Availability</span></Tab>
-      </TabList>
+class AnnualReview extends Component {
+  render() {
+    const { annualReviewData } = this.props;
+    if (!annualReviewData) {
+      return null;
+    }
+    return (
+      <Tabs>
+        <TabList className="nav nav-tabs">
+          <Tab key="Introduction"><span>Introduction</span></Tab>
+          <Tab key="AssetsAndLiabilities"><span>Assets and Liability Statement</span></Tab>
+          <Tab key="FamilyTree"><span>Family Tree</span></Tab>
+          <Tab key="BudgetPlanner"><span>Budget Planner</span></Tab>
+          <Tab key="RiskProfile"><span>Risk Profile</span></Tab>
+          <Tab key="Availability"><span>Availability</span></Tab>
+        </TabList>
 
-      <TabPanel key="Introduction" className="tab-content">
-        <h1>Welcome</h1>
-        <p>The annual review process begins here. Once you've checked the following details we will be able to meet you and provide some expert advice.</p>
-        <p>This annual review is due on: <span>{annualReviewData.due_date}</span></p>
-        <p>Status: <span style={{ color: annualReviewData.status === 'complete' ? 'green' : 'red' }}>{annualReviewData.status}</span></p>
-      </TabPanel>
-      <TabPanel key="AssetsAndLiabilities" className="tab-content">
-        <AssetsAndLiabilities data={annualReviewData.assets_and_liabilities} />
-      </TabPanel>
-      <TabPanel key="FamilyTree" className="tab-content">
-        <FamilyTree data={annualReviewData.family_tree} />
-      </TabPanel>
-      <TabPanel key="BudgetPlanner" className="tab-content">
-        <BudgetPlanner data={annualReviewData.budget_planner} />
-      </TabPanel>
-      <TabPanel key="RiskProfile" className="tab-content">
-        <RiskProfile data={annualReviewData.risk_profile} />
-      </TabPanel>
-      <TabPanel key="Availability" className="tab-content">
-        <Availability data={annualReviewData.availability} />
-      </TabPanel>
-    </Tabs>
-  );
+        <TabPanel key="Introduction" className="tab-content">
+          <h1>Welcome</h1>
+          <p>
+            The annual review process begins here. Once you&apos;ve checked the following details we
+            will be able to meet you and provide some expert advice.
+          </p>
+          <p>This annual review is due on: <span>{annualReviewData.due_date}</span></p>
+          <p>Status: <span style={{ color: annualReviewData.status === 'complete' ? 'green' : 'red' }}>{annualReviewData.status}</span></p>
+        </TabPanel>
+        <TabPanel key="AssetsAndLiabilities" className="tab-content">
+          <AssetsAndLiabilities data={annualReviewData.assets_and_liabilities} />
+        </TabPanel>
+        <TabPanel key="FamilyTree" className="tab-content">
+          <FamilyTree data={annualReviewData.family_tree} />
+        </TabPanel>
+        <TabPanel key="BudgetPlanner" className="tab-content">
+          <BudgetPlanner data={annualReviewData.budget_planner} />
+        </TabPanel>
+        <TabPanel key="RiskProfile" className="tab-content">
+          <RiskProfile data={annualReviewData.risk_profile} />
+        </TabPanel>
+        <TabPanel key="Availability" className="tab-content">
+          <Availability data={annualReviewData.availability} />
+        </TabPanel>
+      </Tabs>
+    );
+  }
 }
 
 AnnualReview.propTypes = {
